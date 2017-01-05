5e4b9ae935673b98e944f108ba5e47af3355cebc
rsister88 <rsister3o0@gmail.com>
2017-01-05 16:00:54 +0100

    Create project
    Create Android Studio Project

---
+++ app/src/main/java/es/vlc_logic/chipremotelcd/MainActivity.java
@@ -0,0 +1,13 @@
+package es.vlc_logic.chipremotelcd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
